2899bc2b98c63a8a5c019cec12e045788fdb4f06
jair <jairdeyner730791@gmail.com>
2022-10-24 19:57:30 -0500

    final

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [SidebarComponent],
+  exports: [SidebarComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+  get historial(): Array<string> {
+    return this.gifsService.historial;
+  }
+
+  constructor(private gifsService: GifsService) {}
+
+  buscar(termino: string): void {
+    this.gifsService.buscarGifs(termino);
+  }
+}
